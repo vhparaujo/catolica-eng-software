991d3a2bcab9a22d1db1b68ea856f1b9c4eb3d2a
vhparaujo <vhugopacheco59@gmail.com>
2022-02-17 19:56:12 -0300

    exercicios terceira aula - condicional

--- Algoritmos/mediaCondicional.c
+++ Algoritmos/mediaCondicional.c
@@ -1,23 +1,30 @@
 #include <stdio.h> // biblioteca para funcionar scanf e printf
-#define MEDIA 9.5
+#define MEDIA 9.5 // definição de constante
 
 int main() { // início
 
-    double nota1, nota2, mediaAluno;
+    double nota1, nota2, mediaAluno; // declaração das variáveis
+    int freq; // declaração das variáveis
 
-    printf("Digite a sua primeira nota: ");
-    scanf("%lf", &nota1);
+    printf("Digite a sua primeira nota: "); // saída para informar usuário
+    scanf("%lf", &nota1); // entrada de dados
 
-    printf("Digite a sua segunda nota: ");
-    scanf("%lf", &nota2);
+    printf("Digite a sua segunda nota: "); // saída para informar usuário
+    scanf("%lf", &nota2); // entrada de dados
 
-    mediaAluno = (nota1 + nota2) / 2;
+    printf("Digite sua frequência de presença de 1 a 100: "); // saída para informar usuário
+    scanf("%d", &freq); // entrada de dados
 
-    printf("Sua média final foi: %.1lf\n", mediaAluno);
+    mediaAluno = (nota1 + nota2) / 2; // cálculo da média simples
 
-    if(mediaAluno > MEDIA)
-        printf("Parabéns! Aprovado!");
-        else printf("Tente novamente! Reprovado!");
+    printf("Sua média final foi: %.1lf\n", mediaAluno); // saída da média simples
+
+    if(mediaAluno >= MEDIA && freq >= 75){ // condicional
+        printf("Parabéns! Aprovado!"); // saída se verdadeiro
+    } else {
+        printf("Tente novamente! Reprovado!"); // saída se falso
+    }
+
+    printf("\n"); // pular última linha
         
-
-} // fim
+} // fi
