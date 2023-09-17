5feaf1b8cef4766eacaeceeab49ac4a9ea063323
vhparaujo <vhugopacheco59@gmail.com>
2023-09-16 21:29:19 -0300

    exercicio 2 da aula 4

--- Programacao Web/aula4/Exercicios/exercicio2.js
+++ Programacao Web/aula4/Exercicios/exercicio2.js
@@ -4,3 +4,32 @@
 // Não é necessário utilizar o prompt, mas querendo se desafiar
 // nestes sentido mandem ver.
 
+const prompt = require('prompt-sync')();
+
+function Aluno(notas) {
+    this.notas = notas.map(parseFloat);
+
+    this.calculaMedia = function() {
+        const somatorio = this.notas.reduce((total, nota) => total + nota);
+        const media = somatorio / this.notas.length;
+        return media;
+    };
+   
+}
+
+function printarResultado(callback){
+    const aprovacao = callback();
+    if(aprovacao >= 7 && aprovacao <= 10){
+        console.log("Aprovado");
+    } else {
+        console.log("Reprovado");
+    }
+}
+
+const input = prompt("Escreva suas notas separadas por vírgula: ");
+const inputNotas = input.split(',');
+
+const aluno1 = new Aluno(inputNotas);
+const mediaAluno1 = aluno1.calculaMedia();
+
+printarResultado(() => mediaAluno1);
