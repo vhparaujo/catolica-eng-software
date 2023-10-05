26b308bfe11cbbdb45369ea72a8186e4216fa3f4
vhparaujo <vhugopacheco59@gmail.com>
2023-10-05 11:19:58 -0300

    adicionado validação de erros

--- Programacao Web/atividades/atividade6/index.js
+++ Programacao Web/atividades/atividade6/index.js
@@ -13,8 +13,30 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => res.render('index.html'));
 
 app.post('/dados', (req, res) => {
-    const {name, address, telefone, date} = req.body;
-    res.render('dados.html', {name, address, telefone, date});
+    let agendamento = {
+        name: req.body.name,
+        address: req.body.address,
+        telefone: req.body.telefone,
+        date: req.body.date
+    };
+
+    let erro_form = false;
+
+    if(agendamento.name == ""){
+        erro_form = true;
+    }
+    if(agendamento.address == ""){
+        erro_form = true;
+    }
+    if(agendamento.telefone == ""){
+        erro_form = true;
+    }
+    if(agendamento.date == ""){
+        erro_form = true;
+    }
+
+    res.render("dados.html", {agendamento, erro_form});
+
 });
 
 app.listen(PORT, () => console.log(`app rodando na porta ${PORT}`));
