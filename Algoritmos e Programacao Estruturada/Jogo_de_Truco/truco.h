01610c6ddcc42cb78d6411f18aad9dcda104a305
Victor Hugo <vhugopacheco59@gmail.com>
2025-03-31 17:25:16 -0300

    primeira versao truco

---
+++ Algoritmos e Programacao Estruturada/Jogo_de_Truco/truco.h
@@ -0,0 +1,16 @@
+// typedef struct {
+//     int valor;
+//     char naipe;
+// } Carta;
+
+// typedef struct {
+//     Carta mao[3];
+//     int pontos;
+// } Jogador;
+
+// void inicializarBaralho(void);
+// void embaralharBaralho(void);
+// void distribuirCartas(void);
+// void mostrarMao(Jogador *jogador);
+// void jogarTurno(Jogador *jogador, int jogador_num);
+// void compararCartas(Carta carta1, Carta carta2);
